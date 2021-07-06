d74489e996103ec5940c9b4a8c8e22f68785f32b
kakuzops <kakuzops@gmail.com>
2021-07-05 22:57:37 -0300

    change elixir.yml

--- config/test.exs
+++ config/test.exs
@@ -9,8 +9,7 @@ config :delivery, Delivery.Repo,
   username: "postgres",
   password: "postgres",
   database: "delivery_test#{System.get_env("MIX_TEST_PARTITION")}",
-  port: String.to_integer(System.get_env("POSTGRES_PORT", "5432"))
-  hostname: System.get_env("POSTGRES_HOST"),
+  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
