06edc58076f3a536fa8ed30c45d9af3d4e6da69e
leonard9 <kakuzops@pop-os.localdomain>
2021-07-12 21:33:34 -0300

    adding some changeset for validate

--- lib/delivery/order.ex
+++ lib/delivery/order.ex
@@ -1,13 +1,26 @@
 defmodule Delivery.Order do
   use Ecto.Schema
+  import Ecto.Changeset
+
   alias Delivery.{OrderProduct}
 
   @primary_key {:id, :binary_id, autogenarate: true}
+
+  @required [
+    :data,
+    :status
+  ]
 
   schema "orders" do
     field :data, :date
     field :status, :integer
 
     has_many :order_products, OrderProduct
+  end
+
+  def changeset(%{} = params) do
+    %__MODULE__{}
+    |> cast(params, @required)
+    |> validate_required(@required)
   end
 end
